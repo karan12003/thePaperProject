c5b0697f811e3b826cbdec8fe62386cf05569742
Karanjot Singh <k.jsingh.12003@gmail.com>
2023-11-18 18:00:56 +0530

    thePaperProject

---
+++ index.js
@@ -0,0 +1,250 @@
+const tl1 = gsap.timeline();
+
+tl1.from('.main', {
+    opacity: 0,
+    y: '0vh',
+    duration:0.1
+})
+    .from(".nav", {
+        opacity: 0,
+        duration: 2,
+        y: -30,
+    })
+    .from(".elem1", {
+        opacity: 0,
+        duration: 2,
+        x: -50,
+        delay: '-2'
+    })
+    .from(".elem2", {
+        opacity: 0,
+        duration: 2,
+        y: 40,
+        delay: '-2'
+    })
+    .from(".elem3", {
+        opacity: 0,
+        duration: 2,
+        x: 50,
+        delay: '-2'
+    })
+    .from(".elems2", {
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: ".elems2",
+            start: "top 60%",
+            end: "bottom 30%",
+            scrub: true,
+        },
+        opacity: 1,
+        scale: 0.3,
+        duration: 3,
+        y: '-100',
+    })
+    .from(".elems3-1-1", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems3-1-1",
+            start: "top 90%",
+            end: "bottom 30%",
+            scrub: true
+        },
+        opacity: 0,
+        x: -150,
+        duration: 2
+    })
+    .from(".img-3", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems3-1-1",
+            start: "top 90%",
+            end: "bottom 30%",
+            scrub: true
+        },
+        opacity: 0,
+        x: -150,
+        duration: 2
+    })
+    .from(".elems3-1-3", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems3-2-2",
+            start: "top 90%",
+            end: "bottom 40%",
+            scrub: true
+        },
+        opacity: 0,
+        x: -100,
+        duration: 2
+    })
+    .from(".img-4", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems3-1-1",
+            start: "top 90%",
+            end: "bottom 50%",
+            scrub: true
+        },
+        opacity: 0,
+        x: 150,
+        duration: 2
+    })
+    .from(".elems3-2-2", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems3-2-2",
+            start: "top 90%",
+            end: "bottom 60%",
+            scrub: true
+        },
+        opacity: 0,
+        x: 100,
+        duration: 2
+    })
+    .from(".website", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".website",
+            start: "top 100%",
+            end: "bottom 70%",
+
+            scrub: true
+        },
+        opacity: 0,
+        scale: 0.5,
+        duration: 2
+    })
+    .from(".stamp", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".website",
+            start: "top 100%",
+            end: "bottom 70%",
+            scrub: true,
+        },
+        opacity: 0,
+        scale: 0.5,
+        duration: 2
+    })
+    .from(".elems4-1-1", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems4-1-1",
+            start: "top 90%",
+            end: "bottom 50%",
+
+            scrub: true,
+        },
+        opacity: 0,
+        scale: 0.5,
+        duration: 2
+    })
+    .from(".elems4-1-2", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems4-1-1",
+            start: "top 90%",
+            end: "bottom 50%",
+
+            scrub: true,
+        },
+        opacity: 0,
+        scale: 0.5,
+        duration: 2
+    })
+    .from(".elems4-2>h1,h2", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems4-1-1",
+            start: "top 90%",
+            end: "bottom 50%",
+            scrub: true,
+        },
+        opacity: 0,
+        x: -150,
+        duration: 2
+    })
+    .from(".elems4-1-3", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems4-1-3",
+            start: "top 90%",
+            end: "bottom 70%",
+            scrub: true,
+        },
+        opacity: 0,
+        x: -190,
+        duration: 2
+    })
+    .from(".elems4-2>span>p", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".elems4-1-3",
+            start: "top 90%",
+            end: "bottom 70%",
+            scrub: true,
+        },
+        opacity: 0,
+        x: -100,
+        duration: 2
+    })
+    .from(".wrk-btn", {
+        scrollTrigger: {
+            scroller: "body",
+            trigger: ".wrk-btn",
+            start: "top 90%",
+            end: "bottom 80%",
+            scrub: true,
+        },
+        opacity: 0,
+        duration: 2,
+        x: -130,
+    })
+    .from(".page5>div>span", {
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: ".page5",
+            start: "top 95%",
+            end: 'bottom 80%',
+            scrub:true
+        },
+        opacity: 0,
+        x: 100,
+        duration: 2
+    })
+    .from(".page5>div>p", {
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: ".page5",
+            start: "top 95%",
+            end: 'bottom 80%',
+            scrub:true
+        },
+        opacity: 0,
+        x: -100,
+        duration: 2
+    })
+    .from(".page6-1", {
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: ".page6",
+            start: "top 100%",
+            end: 'bottom 90%',
+            scrub:true
+        },
+        opacity: 0,
+        x: 150,
+        duration: 2
+    })
+    .from(".page6-2", {
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: ".page6",
+            start: "top 100%",
+            end: 'bottom 90%',
+            scrub:true
+        },
+        opacity: 0,
+        x: -150,
+        duration: 2
+    })
